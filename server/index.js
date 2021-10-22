895dfab605f0ef642840907098eb19c9acdd4888
Shimonbrain <Shimonchouhan789@gmail.com>
2021-10-22 10:47:31 +0530

    babel modules

---
+++ server/index.js
@@ -0,0 +1,3 @@
+import test from './test';
+test("hello user how r u")
+console.log("hello");
---
+++ server/test.js
@@ -0,0 +1,13 @@
+const test=(msg)=>{
+    console.log(msg);
+}
+
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+  res.end('Hello from the server');
+}).listen(4001);
+
+console.log('Server is up and running');
+
+export default test;
